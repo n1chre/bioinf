235dd2f1e54aab9ea5d4f86788d4edf82482320a
Filip Hrenić <hrenic.filip@gmail.com>
2016-12-29 02:09:04 +0100

    implemented binary rank and select using popcount; bitmask_bitset done

--- bitset.cpp
+++ bitset.cpp
@@ -4,9 +4,32 @@
 //
 
 #include "bitset.h"
+#include <iostream>
+
+uint8_t *bitset::generate_bit_lookup(void) {
+  uint8_t *arr = new uint8_t[0xFFFF + 1];
+
+  for (uint32_t i = 0; i <= 0xFFFF; i++) {
+    uint32_t x = i;
+    uint8_t count;
+    for (count = 0; x; count++) {
+      x &= x - 1;
+    }
+    arr[i] = count;
+  }
+
+  return arr;
+}
+
+uint8_t *bitset::BIT_LOOKUP = bitset::generate_bit_lookup();
+
+uint8_t bitset::popcount(uint64_t x) {
+  return BIT_LOOKUP[x >> 48] + BIT_LOOKUP[x >> 32 & 0xFFFF]
+      + BIT_LOOKUP[x >> 16 & 0xFFFF] + BIT_LOOKUP[x & 0xFFFF];
+}
 
 const std::pair<uint32_t, uint32_t> bitset::bits_position(uint32_t idx) {
-  return {idx / unit_size, idx % unit_size};
+  return {idx/unit_size, idx%unit_size};
 }
 
 bitset::bitset(uint32_t _size, uint64_t _val) : __size(_size) {
@@ -14,7 +37,7 @@ bitset::bitset(uint32_t _size, uint64_t _val) : __size(_size) {
   uint32_t size = p.first + 1;
 
   bits.assign(size, 0ULL);
-  if (size != 1 || _size == unit_size) {
+  if (size!=1 || _size==unit_size) {
     bits[0] = _val;
   } else {
     bits[0] = _val & ((1LLU << _size) - 1);
@@ -26,8 +49,48 @@ bitset::bitset(const bitset &bs) {
   this->bits = bs.bits;
 }
 
-uint32_t bitset::popcount(uint32_t idx) const {
-  return 0;
+uint32_t bitset::binary_rank(uint32_t idx) const {
+  uint32_t sol = 0;
+  auto pos = bits_position(idx);
+  for (int i = 0; i < pos.first; ++i) {
+    sol += popcount(bits[i]);
+  }
+  sol += popcount(bits[pos.first] & (1ull << pos.second) - 1);
+  return sol;
+}
+
+uint32_t bitset::binary_select(uint32_t idx, bool v) const {
+  uint32_t sum = 0;
+
+  auto f = [=](uint64_t x) -> uint8_t { return v ? popcount(x) : unit_size - popcount(x); };
+
+  uint32_t i = 0;
+  uint32_t val;
+
+  do {
+    val = f(bits[i]);
+    if (sum + val > idx) { break; }
+    sum += val;
+    ++i;
+  } while (i < bits.size());
+
+  if (i==bits.size()) {
+    throw -1;
+  }
+
+//  std::cout << val << std::endl;
+
+  uint32_t j;
+  for (j = 0; j < unit_size; j++) {
+    if (((bool) (bits[i] & (1llu << j)))==v) { sum++; }
+    if (sum > idx) { break; }
+  }
+
+  if (j==unit_size) {
+    throw -2;
+  }
+
+  return i*unit_size + j;
 }
 
 bitset bitset::operator>>(uint32_t pos) const {
@@ -38,7 +101,7 @@ bitset bitset::operator>>(uint32_t pos) const {
   for (auto i = discard_data.first; i < ret.bits.size(); ++i) {
     auto curr = ret.bits.at(i);
     uint64_t next;
-    if (i + 1 == ret.bits.size()) {
+    if (i + 1==ret.bits.size()) {
       next = 0LLU;
     } else {
       next = ret.bits.at(i + 1);
--- bitset.h
+++ bitset.h
@@ -10,11 +10,19 @@
 
 class bitset {
 
+
  private:
+
+  // popcount
+  static uint8_t *generate_bit_lookup(void);
+  static uint8_t *BIT_LOOKUP;
+  static uint8_t popcount(uint64_t x);
+
   std::vector<uint64_t> bits;
   uint32_t __size;
   static const std::pair<uint32_t, uint32_t> bits_position(uint32_t idx);
-  static const uint32_t unit_size = sizeof(uint64_t) * 8;
+  static const uint8_t unit_size = sizeof(uint64_t)*8;
+
  public:
 
   class bool_proxy {
@@ -32,7 +40,8 @@ class bitset {
   bitset(uint32_t _size, uint64_t _val = 0LLU);
   bitset(const bitset &bs);
 
-  uint32_t popcount(uint32_t idx) const;
+  uint32_t binary_rank(uint32_t idx) const;
+  uint32_t binary_select(uint32_t idx, bool v) const;
 
   bitset operator>>(uint32_t pos) const;
   bool_proxy &operator[](const uint32_t idx);
