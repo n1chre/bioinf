caf5484120f92a4de79d7caa08c9f1713ddc8a42
Filip Hrenić <hrenic.filip@gmail.com>
2016-12-30 20:35:30 +0100

    [script] generate commands

---
+++ scripts/gen_cmds.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+if [[ $# -lt 1 ]] || [[ $# -gt 3 ]]; then
+    echo "usage: $0 length [num_cmds=64] [alphabet=ACTG]"
+    exit 1
+fi
+
+# store parameters
+LENGTH=$1
+
+NUM_CMDS=64
+if [[ $# -eq 2 ]]; then
+    NUM_CMDS=$2
+fi;
+
+ALPHA='ACTG'
+if [[ $# -eq 3 ]]; then
+    ALPHA=$3
+fi;
+
+rand_char() {
+    i=$(( RANDOM % 4 ))
+    echo -n ${ALPHA:${i}:1}
+}
+
+if [[ ${NUM_CMDS} -gt 0 ]]; then
+    for i in `seq 0 ${NUM_CMDS}`; do
+        if [[ $((RANDOM%2)) -eq 0 ]]; then
+            OPERATION='r'
+        else
+            OPERATION='s'
+        fi
+        echo -n ${OPERATION} ' '
+        rand_char
+        echo ' ' $((RANDOM%${LENGTH}))
+    done
+fi
+
