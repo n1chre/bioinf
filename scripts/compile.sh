263ebf5161f11eb2fe75df08cbe1c1ba2e40a8a9
Filip Hrenić <hrenic.filip@gmail.com>
2017-01-03 14:16:45 +0100

    move source files to src folder

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-SOURCE_FILES="bitset.cpp bitset.h bitmask.cpp bitmask.h bitmask_vector.cpp bitmask_vector.h
+SOURCE_FILES=" bitset.cpp bitset.h bitmask.cpp bitmask.h bitmask_vector.cpp bitmask_vector.h
 bitmask_bitset.cpp bitmask_bitset.h wavelet.cpp wavelet.h rank_select.h rb_node.cpp rb_node.h
 rb_tree.cpp rb_tree.h lookup_list.cpp lookup_list.h data.cpp data.h"
+
+SOURCE_FILES="src/$(echo ${SOURCE_FILES} | sed -E 's/ / src\//g')"
+
+FLAGS="-Ofast -march=native -flto -frename-registers -freciprocal-math -fno-signed-zeros -fno-trapping-math -funroll-loops -fassociative-math"
 
 if [[ -f bioinf ]]; then
     rm -f bioinf
@@ -12,5 +16,7 @@ if [[ -f bioinf-test ]]; then
     rm -f bioinf-test
 fi
 
-g++-5 -std=c++14 -Ofast -march=native -flto -frename-registers -freciprocal-math -fno-signed-zeros -fno-trapping-math -funroll-loops  -fassociative-math -o bioinf main.cpp ${SOURCE_FILES}
-g++-5 -std=c++14 -Ofast -march=native -flto -frename-registers -freciprocal-math -fno-signed-zeros -fno-trapping-math -funroll-loops  -fassociative-math -o bioinf-test test.h test.cpp ${SOURCE_FILES}
+cd
+
+g++-5 -std=c++14 ${FLAGS} -o bioinf src/main.cpp ${SOURCE_FILES}
+g++-5 -std=c++14 ${FLAGS} -o bioinf-test src/test.h src/test.cpp ${SOURCE_FILES}
